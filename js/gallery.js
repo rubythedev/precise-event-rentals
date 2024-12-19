8b861de6e2e8b0015f946cc0ceee5592d4920013
Ruby Nunez <rubyn0620@gmail.com>
2024-12-18 22:36:39 -0500

    Update main

--- js/gallery.js
+++ js/gallery.js
@@ -70,4 +70,17 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector(".hamburger");
+  const menuWrapper = document.querySelector(".menu-wrapper");
+
+  // Toggle classes on click
+  hamburgerMenu.addEventListener("click", () => {
+    menuWrapper.classList.toggle("open");
+    hamburgerMenu.classList.toggle("open");
+  });
+});
+
   
--- js/script.js
+++ js/script.js
@@ -155,3 +155,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initialize the gallery
   updateGallery();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.querySelector(".hamburger");
+  const menuWrapper = document.querySelector(".menu-wrapper");
+
+  // Toggle classes on click
+  hamburgerMenu.addEventListener("click", () => {
+    menuWrapper.classList.toggle("open");
+    hamburgerMenu.classList.toggle("open");
+  });
+});
